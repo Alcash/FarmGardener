bfe230b962118356af4b263ea20321c58eb14fa5
Andrey.Inozemtsev <Pirynas@mail.ru>
2022-11-17 16:51:45 +0700

    доработка

--- Assets/Project/Scripts/UI/MainMenuController.cs
+++ Assets/Project/Scripts/UI/MainMenuController.cs
@@ -1,4 +1,5 @@
 using EventManager;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,10 +12,28 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private Slider m_WidthSlider;
     [SerializeField] private Slider m_HeightSlider;
 
+    [SerializeField] private Text m_WidthText;
+    [SerializeField] private Text m_HeightText;
+
     private void Start()
     {
 
         m_ButtonStart.onClick.AddListener(ButtonStartClicked);
+        m_WidthSlider.onValueChanged.AddListener(SliderWidthChangeValue);
+        m_HeightSlider.onValueChanged.AddListener(SliderHeightChangeValue);
+        m_HeightText.text = m_HeightSlider.value.ToString();
+        m_WidthText.text = m_WidthSlider.value.ToString();
+        
+    }
+
+    private void SliderHeightChangeValue(float value)
+    {
+        m_HeightText.text = value.ToString();
+    }
+
+    private void SliderWidthChangeValue(float value)
+    {
+        m_WidthText.text = value.ToString();
     }
 
     private void ButtonStartClicked()
@@ -22,5 +41,9 @@ public class MainMenuController : MonoBehaviour
         gameObject.SetActive(false);
         EventManager.EventManager.SendMessage(new StartGameMessage((int)m_HeightSlider.value, (int)m_WidthSlider.value));
     }
+
+   
+
+    
 
 }
